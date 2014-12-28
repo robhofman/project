50036cda2ce6ebf2c2cae61999302c631b35408f
robhofman <rob.hofman@student.howest.be>
2014-12-28 22:07:58 +0100

    appart project gemaakt voor klant
    problemen met de connectionstrings oplossen.

---
+++ nmct.ba.cashlessproject/nmct.ba.cashlessproject.klant.ui/ViewModel/KlantHerlaadVM.cs
@@ -0,0 +1,17 @@
+﻿using nmct.ba.cashlessproject.klant.ui.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nmct.ba.cashlessproject.ui.ViewModel
+{
+    class KlantHerlaadVM: ObservableObject, IPage
+    {
+        public string Name
+        {
+            get { return "herlaad page"; }
+        }
+    }
+}
